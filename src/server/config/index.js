2c8f33dc0cfc0d3cfbf956a7c5f67f33a76bdc02
Danil <danil.kazim.99@gmail.com>
2018-01-24 10:12:16 +0200

    Set: +root url production

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    ROOT_URL: (process.env.NODE_ENV == 'dev') ? `localhost:${this.PORT}` : '',
+    ROOT_URL: (process.env.NODE_ENV == 'dev') ? `localhost:${this.PORT}` : 'https://meme-picker.herokuapp.com',
     DB_URL: (process.env.NODE_ENV == 'dev') ?
         `mongodb://${process.env.DB_LOGIN_DEV}:${process.env.DB_PSW_DEV}@ds261527.mlab.com:61527/int20test_dev` :
         `mongodb://${process.env.DB_LOGIN_PROD}:${process.env.DB_SWP_PROD}@ds046677.mlab.com:46677/int20test`,
@@ -14,5 +14,5 @@ module.exports = {
         SERVER_SALT:process.env.SERVER_SALT,
     },
     PAGINATION_LIMIT: process.env.PAGINATION_LIMIT || 25,
-    VIEW_URL:(process.env.NODE_ENV == 'dev') ? `localhost:8080` : '',
+    VIEW_URL:(process.env.NODE_ENV == 'dev') ? `localhost:8080` : 'https://meme-picker.herokuapp.com',
 };
