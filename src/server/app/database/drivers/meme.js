956521ca4730ec514e2e3d5fe84c6066b4427874
IceBroForever <pasha1ruh@gmail.com>
2018-01-24 12:58:12 +0200

    adding memes

--- src/server/app/database/drivers/meme.js
+++ src/server/app/database/drivers/meme.js
@@ -3,37 +3,8 @@
 const DB = require('@DB');
 const Meme = require('@DBschemas/meme');
 
-// const NineGag = require('9gag');
-// const Scraper = NineGag.Scraper;
-// const NumOfPosts = 50;
-
-// async function getNewMemes() {
-//     const scraper = new Scraper(NumOfPosts);
-//     let added = 0;
-
-//     try {
-//         const posts = await scraper.scrap();
-
-//         for (let post of posts) {
-//             let data = {
-//                 title: post.title,
-//                 content: post.content
-//             };
-
-//             await meme.create(data);
-
-//             added++;
-//         }
-
-//         console.log(`Added ${NumOfPosts} memes to DB`);
-//     }
-//     catch (err) {
-//         if (added != 0) console.log(`Added ${added} memes to DB`);
-//     }
-// }
-
 async function create (data) {
-    if (await DB.methods.get.oneByQuery(data)) throw new Error('Meme is already exist');
+    if (await DB.methods.get.oneByQuery(Meme, data)) throw new Error('Meme is already exist');
 
     return await DB.methods.create(Meme, data);
 }
--- src/server/app/services/index.js
+++ src/server/app/services/index.js
@@ -1,8 +1,40 @@
 const mongoose = require('mongoose');
 const config = require('@config');
-const auth=require('@services/auth');
+const auth = require('@services/auth');
 const emailVarification = require('@services/emailVarification');
-function connectDB () {
+
+const NineGag = require('9gag'),
+    Scraper = NineGag.Scraper,
+    NumOfPosts = 50;
+
+const meme = require('@DB').Meme;
+
+async function addNewMemesToDB() {
+    const scraper = new Scraper(NumOfPosts);
+    let added = 0;
+
+    try {
+        const posts = await scraper.scrap();
+
+        for (let post of posts) {
+            let data = {
+                title: post.title,
+                content: post.content
+            };
+
+            await meme.create(data);
+
+            added++;
+        }
+
+        console.log(`Added ${NumOfPosts} memes to DB`);
+    }
+    catch (err) {
+        if (added != 0) console.log(`Added ${added} memes to DB`);
+    }
+}
+
+function connectDB() {
     const database = mongoose.connection;
     mongoose.Promise = Promise;
     mongoose.connect(config.DB_URL, {
@@ -11,7 +43,9 @@ function connectDB () {
     });
     database.on('error', error => console.log(`-DB: connection failed: ${error}`));
     database.on('connected', async () => {
-        console.log(`+DB: connected to ${config.DB_URL}`)
+        console.log(`+DB: connected to ${config.DB_URL}`);
+        addNewMemesToDB();
+        setInterval(() => { addNewMemesToDB() }, 2 * 1000 * 60 * 60);
     });
     database.on('disconnected', () => console.log('-DB: disconnected'));
     process.on('SIGINT', () => {
