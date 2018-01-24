ed164f3ad22ac9d89c64d9e38ce8ba4c822ea45d
IceBroForever <pasha1ruh@gmail.com>
2018-01-24 02:25:42 +0200

    Email verification

--- src/server/app/services/emailVarification.js
+++ src/server/app/services/emailVarification.js
@@ -1,3 +1,16 @@
-module.exports = (app) => {
-    
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport(`smpts://${process.env.EMAIL}:${process.env.EMAIL_PASSWORD}@smtp.google.com`);
+
+module.exports = {
+    sendVerification: (email, token, url) => {
+        let mail = {
+            from: "Meme comparator",
+            to: email,
+            subject: "Confirm registration",
+            html: `<a href="${url}/activate/${token}">Click to confirm your email</a>`
+        };
+
+        transporter.sendMail(mail, (err, info) => console.log(err));
+    }
 }
